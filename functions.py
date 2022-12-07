42ece424dafb21b2c3b7388c52e04f9e6e3b3fed
StevenDelval <asteven59220@free.fr>
2022-12-07 14:57:46 +0100

    create function correspondance

---
+++ functions.py
@@ -0,0 +1,13 @@
+def correspondance(pays_code):
+    if pays_code == "fr_FR":
+        return "France"
+    if pays_code == "en_US":
+        return "Etat Unie"
+    if pays_code == "es_ES":
+        return "Espagne"
+    if pays_code == "it_IT":
+        return "Italie"
+    if pays_code == "ja_JP":
+        return "Japon"
+    if pays_code == "en_GB":
+        return "Grande Bretagne"
