255f40dfaad53fc66069f56b1166a496f4b23c44
StevenDelval <asteven59220@free.fr>
2022-12-07 16:35:16 +0100

    make data for employeTable

--- makedata.py
+++ makedata.py
@@ -6,17 +6,22 @@ from create_db import *
 from functions import *
 import random
 
+random.seed(0)
+Faker.seed(0)
+
 db_url = "sqlite:///bdd_restaurant.db"
 engine = create_engine(db_url)
 Session = sessionmaker(bind=engine) 
 session = Session()
 
+## Remplire table carte
 faker_arg = ["fr_FR", "en_US","es_ES","it_IT","ja_JP","en_GB"]
 for country in faker_arg:
     ligne = CarteTable(pays = correspondance(country))
     session.add(ligne)
     session.commit()
 
+## Remplire table restaurant
 for arg in faker_arg:
     fake = Faker(arg)
     for nb in range(random.randint(1,60)):
@@ -27,6 +32,40 @@ for arg in faker_arg:
             )
         session.add(ligne)
         session.commit()
+
+## Remplire table employe
+for restaurant in session.query(RestaurantTable).all():
+    fake = Faker(inv_correspondance(restaurant.pays))
+    ligne= EmployeTable(id_restaurant=restaurant.id_restaurant,\
+        nom=fake.last_name(),prenom=fake.first_name(),\
+        adresse=fake.street_address(),experience=random.randint(0,30),\
+        poste="directeur",note=random.randint(8,10),date_entree=str(fake.date_object()))
+    session.add(ligne)
+    session.commit()
+    id_directeur = session.query(EmployeTable).all()[-1].id_employe
+    for manager in range(random.randint(0,5)):
+        ligne= EmployeTable(id_superviseur=id_directeur,id_restaurant=restaurant.id_restaurant,\
+            nom=fake.last_name(),prenom=fake.first_name(),\
+            adresse=fake.street_address(),experience=random.randint(0,30),\
+            poste="manager",note=random.randint(1,10),date_entree=str(fake.date_object()))
+        session.add(ligne)
+        session.commit()
+        id_manager = session.query(EmployeTable).all()[-1].id_employe
+        for employe in range(random.randint(0,10)):
+            poste = random.choice(["caissiers","cuisiniers"])
+            ligne= EmployeTable(id_superviseur=id_manager,id_restaurant=restaurant.id_restaurant,\
+                nom=fake.last_name(),prenom=fake.first_name(),\
+                adresse=fake.street_address(),experience=random.randint(0,30),\
+                poste=poste,note=random.randint(1,10),date_entree=str(fake.date_object()))
+            session.add(ligne)
+            session.commit()
+
+
+
+
+
+
+    
 
 
 
