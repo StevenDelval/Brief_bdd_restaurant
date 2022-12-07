a040f689cbb4cae13a60aabb3a198bb03a724a21
StevenDelval <asteven59220@free.fr>
2022-12-07 16:45:51 +0100

    add comment

--- makedata.py
+++ makedata.py
@@ -36,6 +36,8 @@ for arg in faker_arg:
 ## Remplire table employe
 for restaurant in session.query(RestaurantTable).all():
     fake = Faker(inv_correspondance(restaurant.pays))
+    
+    #### Creation directeur pour le restaurant
     ligne= EmployeTable(id_restaurant=restaurant.id_restaurant,\
         nom=fake.last_name(),prenom=fake.first_name(),\
         adresse=fake.street_address(),experience=random.randint(0,30),\
@@ -43,6 +45,9 @@ for restaurant in session.query(RestaurantTable).all():
     session.add(ligne)
     session.commit()
     id_directeur = session.query(EmployeTable).all()[-1].id_employe
+
+    
+    #### Creation des manager pour le directeur
     for manager in range(random.randint(0,5)):
         ligne= EmployeTable(id_superviseur=id_directeur,id_restaurant=restaurant.id_restaurant,\
             nom=fake.last_name(),prenom=fake.first_name(),\
@@ -51,6 +56,9 @@ for restaurant in session.query(RestaurantTable).all():
         session.add(ligne)
         session.commit()
         id_manager = session.query(EmployeTable).all()[-1].id_employe
+        
+        
+    #### Creation des employe pour le manager
         for employe in range(random.randint(0,10)):
             poste = random.choice(["caissiers","cuisiniers"])
             ligne= EmployeTable(id_superviseur=id_manager,id_restaurant=restaurant.id_restaurant,\
@@ -59,9 +67,6 @@ for restaurant in session.query(RestaurantTable).all():
                 poste=poste,note=random.randint(1,10),date_entree=str(fake.date_object()))
             session.add(ligne)
             session.commit()
-
-
-
 
 
 
