56bbdae5e6316d29a4425f054577884b3660e883
StevenDelval <asteven59220@free.fr>
2022-12-08 10:30:58 +0100

    fonction loading

--- functions.py
+++ functions.py
@@ -1,3 +1,21 @@
+import os
+from math import ceil
+import random
+
+random.seed(0)
+
+def loading(percent):
+    """
+    Fonction qui nettoye le terminal.
+    """
+    my_os = os.name
+    if my_os == "posix":
+        os.system('clear')
+    else:
+        os.system('cls')
+    print('[ ' + '#'*ceil(20*(percent)/100) + ' '*ceil(20*(100-percent)/100) + ' ]')
+
+
 def correspondance(pays_code):
     if pays_code == "fr_FR":
         return "France"
@@ -25,3 +43,15 @@ def inv_correspondance(pays):
         return "ja_JP"
     if pays == "Grande Bretagne":
         return "en_GB"
+
+def salaire_poste(poste):
+    if poste == "directeur":
+        return random.uniform(4000,5000)
+    if poste == "manager":
+        return random.uniform(2000,4000)
+    if poste == "caissiers":
+        return random.uniform(1000,1800)
+    if poste == "cuisiniers":
+        return random.uniform(1200,2000)
+    
+
