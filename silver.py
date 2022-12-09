500c62262294fda43d5d0df09bb6bf8c9305d3ed
StevenDelval <asteven59220@free.fr>
2022-12-09 17:01:59 +0100

    question 2

--- silver.py
+++ silver.py
@@ -17,8 +17,6 @@ engine_silver = create_engine(db_url_silver)
 if not database_exists(engine_silver.url):
     create_database(engine_silver.url)
 
-Session_silver = sessionmaker(bind=engine_silver) 
-session_silver = Session()
 
 metadata_silver = MetaData(engine_silver)
 if not "employe_dep" in metadata_silver.tables:  # If table don't exist, Create.
@@ -49,5 +47,83 @@ with engine.begin() as con:
             con_silver.execute(ins)
 
 
+if not "ingredient_star_dep_par_item" in metadata_silver.tables:  # If table don't exist, Create.
+    metadata_silver = MetaData(engine_silver)
+    # Create a table with the appropriate Columns
+    ingredient_star_dep_par_item=Table("ingredient_star_dep_par_item", metadata_silver,
+          Column('id', Integer, primary_key=True, nullable=False),
+          Column('date', String),
+          Column('pays', String),
+          Column('dep', String),
+          Column('nom_ingredient',String),
+          Column('nb_ingredient',Integer)
+          )
+    # Implement the creation
+    metadata_silver.create_all()
 
-    
+with engine.begin() as con:
+
+    request = con.execute("SELECT pays,code_postal,SUM(Recette.quantite*ItemInTicket.quantite) as nb_utiliser,Ingredient.nom FROM Restaurant\
+            JOIN Ticket ON Ticket.id_restaurant= Restaurant.id_restaurant\
+            JOIN ItemInTicket ON Ticket.id_ticket=ItemInTicket.link_ticket \
+            JOIN  Item ON Item.id_item=ItemInTicket.link_item \
+            JOIN Recette ON Item.id_item=Recette.link_item \
+            JOIN  Ingredient ON Recette.link_ingredient=Ingredient.id_ingredient \
+            GROUP BY pays,code_postal,Ingredient.nom\
+            ORDER BY pays,code_postal,nb_utiliser DESC")
+    id=0
+    for row in request:
+        
+        with engine_silver.begin() as con_silver:
+            
+            if row[1] != id:
+                ins = ingredient_star_dep_par_item.insert().values(date =date_time_str , pays = row[0],dep=row[1][0:2],nom_ingredient=row[3],nb_ingredient=row[2])
+
+                con_silver.execute(ins)
+                id = row[1]
+
+
+if not "ingredient_star_dep_par_menu" in metadata_silver.tables:  # If table don't exist, Create.
+    metadata_silver = MetaData(engine_silver)
+    # Create a table with the appropriate Columns
+    ingredient_star_dep_par_menu=Table("ingredient_star_dep_par_menu", metadata_silver,
+          Column('id', Integer, primary_key=True, nullable=False),
+          Column('date', String),
+          Column('pays', String),
+          Column('dep', String),
+          Column('nom_ingredient_plat',String),
+          Column('nb_ingredient_plat',Integer),
+          Column('nom_ingredient_boisson',String),
+          Column('nb_ingredient_boisson',Integer),
+          Column('nom_ingredient_dessert',String),
+          Column('nb_ingredient_dessert',Integer),
+          )
+    # Implement the creation
+    metadata_silver.create_all()
+
+with engine.begin() as con:
+
+    request = con.execute("""SELECT pays,code_postal,SUM(rec_plat.quantite*MenuInTicket.quantite) as nb_utiliser_plat,ingre_plat.nom as plat,SUM(rec_boisson.quantite*MenuInTicket.quantite) as nb_utiliser_boisson,ingre_boisson.nom as boisson,SUM(rec_dessert.quantite*MenuInTicket.quantite) as nb_utiliser_dessert,ingre_dessert.nom as dessert FROM Restaurant
+            JOIN Ticket ON Ticket.id_restaurant= Restaurant.id_restaurant
+            JOIN MenuInTicket ON Ticket.id_ticket=MenuInTicket.link_ticket 
+            JOIN  Menu ON Menu.id_menu=MenuInTicket.link_menu 
+            JOIN  Item as plat ON plat.id_item=Menu.plat 
+            JOIN  Item as boisson ON boisson.id_item=Menu.boisson 
+            JOIN  Item as dessert ON dessert.id_item=Menu.dessert 
+            JOIN Recette as rec_plat ON plat.id_item=rec_plat.link_item 
+            JOIN Recette as rec_boisson ON boisson.id_item=rec_boisson.link_item
+            JOIN Recette as rec_dessert ON dessert.id_item=rec_dessert.link_item
+            JOIN  Ingredient as ingre_plat ON rec_plat.link_ingredient=ingre_plat.id_ingredient 
+            JOIN  Ingredient as ingre_boisson ON rec_boisson.link_ingredient=ingre_boisson.id_ingredient 
+            JOIN  Ingredient as ingre_dessert ON rec_dessert.link_ingredient=ingre_dessert.id_ingredient 
+            GROUP BY pays,code_postal,plat,boisson,dessert
+            ORDER BY code_postal,nb_utiliser_plat DESC,nb_utiliser_boisson DESC,nb_utiliser_dessert DESC """)
+    id=0
+    for row in request:
+        with engine_silver.begin() as con_silver:
+            
+            if row[1] != id:
+                ins = ingredient_star_dep_par_menu.insert().values(date =date_time_str , pays = row[0],dep=row[1][0:2],nom_ingredient_plat=row[3],nb_ingredient_plat=row[2],nom_ingredient_boisson=row[5],nb_ingredient_boisson=row[4],nom_ingredient_dessert=row[7],nb_ingredient_dessert=row[6])
+
+                con_silver.execute(ins)
+                id = row[1]
